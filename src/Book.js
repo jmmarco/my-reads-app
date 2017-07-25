3ee6328e82cef472834f63985fdec132b0c1ca43
Juan Martin Marco <jmmarco@gmail.com>
2017-07-25 14:06:03 -0300

    Playing with Book component

--- src/Book.js
+++ src/Book.js
@@ -2,46 +2,78 @@ import React, { Component } from 'react'
 import * as BooksAPI from './utils/BooksAPI'
 
 class Book extends Component {
-
   state = {
-    book : null,
-    bookId: null,
+    book: null,
     shelf: '',
     value: 'none'
-
   }
 
-  getBook = (bookId) => {
-    console.log(bookId)
-    BooksAPI.get(bookId)
-    .then((book) => {
-      // do something
+
+
+
+  componentDidMount() {
+    BooksAPI.get(this.props.book.id).then((book) => {
       this.setState({
-        book
+        book: book,
+        shelf: book.shelf,
+        value: book.shelf
       })
-
-      console.log("State was set")
-      console.log(this.state)
     })
-    .catch(err => console.log('Something went wrong', err))
-
   }
 
 
   updateBook = (event) => {
 
-    console.log(event)
-
-    BooksAPI.update(this.state.book, this.state.shelf)
-    console.log(this.state.book, this.state.shelf)
-    .then((book, shelf) => {
-      this.setState({
-        value: event.target.value
-      })
-      console.log(this.state)
+    BooksAPI.update(this.state.book, event.target.value).then((book) => {
+      this.setState((state) => ({
+        book: state.book,
+        shelf: state.book.shelf,
+        value: state.book.shelf
+      }))
     })
-    .catch(err => console.log('Something went wrong', err))
+
   }
+
+  handleChange(event) {
+
+    BooksAPI.update(this.state.book, event.target.value).then((book) => {
+      console.log(book)
+      this.setState({
+
+        value: book.shelf
+      })
+    }, () => { // Callback to check if the value is set
+      console.log('Value is now: ', this.state.value)
+       console.log('State is: ', this.state)
+    })
+
+    console.log(this.state)
+
+  }
+
+
+
+
+
+    // console.log(this.state)
+    // console.log(this.props)
+    // this.setState({
+    //   book: this.props.book,
+    //   value: event.target.value,
+    // }, () => { // Callback to check if the value is set
+    //   console.log('Value is now: ', this.state.value)
+    //   console.log('State is: ', this.state)
+    // })
+    //
+    // console.log(this.state)
+
+
+
+
+
+
+
+
 
   render() {
     const {book} = this.props
@@ -54,7 +86,7 @@ class Book extends Component {
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail })` }}></div>
                 <div className="book-shelf-changer">
-                  <select value={this.state.value} onChange={(event) => this.updateBook}>
+                  <select value={this.state.value} onChange={(event) => this.updateBook(event)}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
