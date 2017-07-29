ad44a90f4ddc938a35301324d1ff62a112f1a6d4
Juan Martin Marco <jmmarco@gmail.com>
2017-07-29 11:50:33 -0300

    Handle more than one author

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class Book extends Component {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+          <div className="book-authors">{book.authors !== undefined && book.authors.length > 1 ? book.authors.map((a) => `${a} `) : book.authors}</div>
         </div>
       </li>
     )
