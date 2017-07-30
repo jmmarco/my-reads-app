4effe3e631e56d7fff90a831c3c08b55ecb73756
Juan Martin Marco <jmmarco@gmail.com>
2017-07-30 16:31:23 -0300

    Activate react router

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root'))
 registerServiceWorker()
